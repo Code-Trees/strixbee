2333a8c4491e5c70f1d2fb6d523c2600c0af670c
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2022-02-16 15:08:15 +0530

    Update clean_gpu.py

--- utils/clean_gpu.py
+++ utils/clean_gpu.py
@@ -3,6 +3,7 @@ warnings.filterwarnings('ignore')
 
 import gc
 import torch
+import os
 
 def clean_gpu():
     """Considering we have GPUs"""
@@ -12,7 +13,7 @@ def clean_gpu():
     return None
     
 def gpu_info():
-    gpu_info = !nvidia-smi
+    gpu_info = os.system("nvidia-smi")
     gpu_info = '\n'.join(gpu_info)
     if gpu_info.find('failed') >= 0:
         print('Not connected to a GPU')
