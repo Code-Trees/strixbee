829d564be35ec225e747c5994f2e4012d78f56a6
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2022-02-17 22:55:27 +0530

    Update optimizer_utils.py
    Updated ReduceLRonPlateau and added LR finder

--- utils/optimizer_utils.py
+++ utils/optimizer_utils.py
@@ -3,17 +3,28 @@ warnings.filterwarnings('ignore')
 
 import torch
 from torch.optim import SGD
-from torch.optim.lr_scheduler import StepLR
+from torch.optim.lr_scheduler import StepLR,ReduceLROnPlateau
+from torch_lr_finder import LRFinder
+import torch.nn as nn
 
-def get_optimizer(model_obj,loss_type=None,scheduler = False):
+def get_optimizer(model_obj,train_loader,loss_type=None,scheduler = False):
     loss_type= str(loss_type).upper()
     parameters = model_obj.parameters()
+
     if loss_type  == 'L2' :
         optimizer = SGD( params = parameters,lr = 0.01,momentum = 0.9,weight_decay= 0.001)
     else:
-        optimizer = SGD( params = parameters,lr = 0.01,momentum = 0.9)
+        optimizer = SGD( params = parameters,lr = 1e-7,momentum = 0.9)
+        criterion = nn.NLLLoss()
+        lr_finder = LRFinder(model_obj,optimizer,criterion,device = 'cuda' if torch.cuda.is_available() else 'cpu')
+        lr_finder.range_test(train_loader,end_lr = 10,num_iter = 100)
+        grapg,lr_rate = lr_finder.plot()
+        lr_finder.reset()
+
+        optimizer = SGD( params = parameters,lr =lr_rate,momentum = 0.9)
+
     if scheduler == True:
-        scheduler = StepLR(optimizer,step_size = 20,gamma = 0.1)
+        scheduler = ReduceLROnPlateau(optimizer,mode = 'min')
         return optimizer,scheduler
     else:
         return optimizer
