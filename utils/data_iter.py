e4172e4e82f1fba7d278bfb93cc9aa26f0f4b5db
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2022-02-17 19:35:28 +0530

    Update data_iter.py

--- utils/data_iter.py
+++ utils/data_iter.py
@@ -4,6 +4,7 @@ from torchvision import transforms,datasets
 from torch.utils.data import DataLoader
 
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 
 def get_data(train_transform_composes =None ,test_transform_composes =None):
