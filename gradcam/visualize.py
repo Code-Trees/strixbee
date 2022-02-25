85138271c76249be6100dad4c9956580b2377885
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2022-02-25 12:57:56 +0530

    Update visualize.py

--- gradcam/visualize.py
+++ gradcam/visualize.py
@@ -7,71 +7,84 @@ from strixbee.gradcam.gradcam import GradCam
 # from CIFAR_10.utils import denormalize
 from strixbee.utils.plots import convert_image_np
 
+def denormalize(tensor, mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]):
+    single_img = False
+    if tensor.ndimension() == 3:
+      single_img = True
+      tensor = tensor[None,:,:,:]
+
+    if not tensor.ndimension() == 4:
+        raise TypeError('tensor should be 4D')
+
+    mean = torch.FloatTensor(mean).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+    std = torch.FloatTensor(std).view(1, 3, 1, 1).expand_as(tensor).to(tensor.device)
+    ret = tensor.mul(std).add(mean)
+    return ret[0] if single_img else ret
+
 class VisualizeCam(object):
 
-    def __init__(self, model, classes, target_layers,mean,std):
-        super(VisualizeCam, self).__init__()
-        self.model = model
-        self.classes = classes
-        self.target_layers = target_layers
-        self.device = next(model.parameters()).device
-        self.mean = mean
-        self.std = std
-        self.gcam = GradCam(model, target_layers, len(classes))
-        
-    def visualize_cam(self, mask, img):
-        heatmap = (255 * mask.squeeze()).type(torch.uint8).cpu().numpy()
-        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-        heatmap = torch.from_numpy(heatmap).permute(2, 0, 1).float().div(255)
-        b, g, r = heatmap.split(1)
-        heatmap = torch.cat([r, g, b])
-        
-        result = heatmap+img.cpu()
-        result = result.div(result.max()).squeeze()
-        return heatmap, result
+	def __init__(self, model, classes, target_layers):
+		super(VisualizeCam, self).__init__()
+		self.model = model
+		self.classes = classes
+		self.target_layers = target_layers
+		self.device = next(model.parameters()).device
 
-    def plot_heatmaps(self, img_data, target_class, img_name):
-        fig, axs = plt.subplots(nrows=2, ncols=5, figsize=(10, 4),
-            subplot_kw={'xticks': [], 'yticks': []})
-        fig.suptitle('GradCam at different conv layers for the class: %s' % 
-            target_class, fontsize=13, weight='medium', y=1.05)
+		self.gcam = GradCam(model, target_layers, len(classes))
+		
+	def visualize_cam(self, mask, img):
+	    heatmap = (255 * mask.squeeze()).type(torch.uint8).cpu().numpy()
+	    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+	    heatmap = torch.from_numpy(heatmap).permute(2, 0, 1).float().div(255)
+	    b, g, r = heatmap.split(1)
+	    heatmap = torch.cat([r, g, b])
+	    
+	    result = heatmap+img.cpu()
+	    result = result.div(result.max()).squeeze()
+	    return heatmap, result
 
-        for ax, data in zip(axs.flat, img_data):
-            img = data["img"]
-            # img = torch.tensor(img)
-            # img = img.cpu().numpy()
-            type(img)
-            # maxValue = np.amax(img)
-            # minValue = np.amin(img)
-            # img = np.clip(img, 0, 1)
-            # img = img/np.amax(img)
-            # img = np.clip(img, 0, 1)
-            ax.imshow(img)
-            ax.set_title("%s" % (data["label"]))
+	def plot_heatmaps(self, img_data, target_class, img_name):
+		fig, axs = plt.subplots(nrows=2, ncols=5, figsize=(10, 4),
+			subplot_kw={'xticks': [], 'yticks': []})
+		fig.suptitle('GradCam at different conv layers for the class: %s' % 
+			target_class, fontsize=13, weight='medium', y=1.05)
 
-    def __call__(self, images, target_layers, target_inds=None, metric=""):
-        masks_map, pred = self.gcam(images, target_layers, target_inds)
-        for i in range(len(images)):
-            img = images[i]
-            results_data = [{
-                "img": convert_image_np(img,self.mean,self.std),
-                "label": "Result:"
-            }]
-            heatmaps_data = [{
-                "img": convert_image_np(img,self.mean,self.std),
-                "label": "Heatmap:"
-            }]
-            for layer in target_layers:
-                mask = masks_map[layer][i]
-                heatmap, result = self.visualize_cam(mask, img)
-                results_data.append({
-                    "img": result,
-                    "label": layer
-                })
-                heatmaps_data.append({
-                    "img": heatmap,
-                    "label": layer
-                })
-            pred_class = self.classes[pred[i][0]]
-            fname = "gradcam_%s_%s_%s.png" % (metric, i, pred_class)
-            self.plot_heatmaps(results_data+heatmaps_data, pred_class, fname)
+		for ax, data in zip(axs.flat, img_data):
+			img = data["img"]
+			img = img.cpu().numpy()
+			maxValue = np.amax(img)
+			minValue = np.amin(img)
+			img = np.clip(img, 0, 1)
+			img = img/np.amax(img)
+			img = np.clip(img, 0, 1)
+			ax.imshow(np.transpose(img, (1, 2, 0)))
+			ax.set_title("%s" % (data["label"]))
+
+
+
+	def __call__(self, images, target_layers, target_inds=None, metric=""):
+		masks_map, pred = self.gcam(images, target_layers, target_inds)
+		for i in range(len(images)):
+			img = images[i]
+			results_data = [{
+				"img": denormalize(img),
+				"label": "Result:"
+			}]
+			heatmaps_data = [{
+				"img": denormalize(img),
+				"label": "Heatmap:"
+			}]
+			for layer in target_layers:
+				mask = masks_map[layer][i]
+				heatmap, result = self.visualize_cam(mask, img)
+				results_data.append({
+					"img": result,
+					"label": layer
+				})
+				heatmaps_data.append({
+					"img": heatmap,
+					"label": layer
+				})
+			pred_class = self.classes[pred[i][0]]
+			fname = "gradcam_%s_%s_%s.png" % (metric, i, pred_class)
+			self.plot_heatmaps(results_data+heatmaps_data, pred_class, fname)
