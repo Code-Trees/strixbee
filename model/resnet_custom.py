83d93a1ba7ab2b3fb88f4d0f829db42638306142
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2022-03-09 11:55:09 +0530

    Update resnet_custom.py

--- model/resnet_custom.py
+++ model/resnet_custom.py
@@ -15,50 +15,59 @@ class Cifar_Net_R(nn.Module):
     def __init__(self,norm_type = 'bn',drop_out = 0.0): 
         super(Cifar_Net_R,self).__init__()
         self.norm_type = norm_type
+        self.dropout_val = drop_out
         
         self.prep_layer = nn.Sequential(
                                         nn.Conv2d(in_channels= 3, out_channels=64 , kernel_size = (3,3), stride = 1, padding = 1,bias= False),
                                         Normalize(norm_type = self.norm_type ,num_features= 64),
-                                        nn.ReLU()
+                                        nn.ReLU(),
+                                        nn.Dropout2d(p = self.dropout_val)
                                         )
         
         self.layer1 = nn.Sequential(    
                                     nn.Conv2d(in_channels= 64, out_channels=128 , kernel_size = (3,3), stride = 1, padding = 1,bias= False),
                                     nn.MaxPool2d(kernel_size= (2,2),stride= 2),
                                     Normalize(norm_type = self.norm_type ,num_features= 128),
-                                    nn.ReLU()
+                                    nn.ReLU(),
+                                    nn.Dropout2d(p = self.dropout_val)
                                     )
         
         self.ResBlock1 = nn.Sequential(    
                                     nn.Conv2d(in_channels= 128, out_channels=128 , kernel_size = (3,3), stride = 1, padding = 1,bias= False),
                                     Normalize(norm_type = self.norm_type ,num_features= 128),
                                     nn.ReLU(),
+                                    nn.Dropout2d(p = self.dropout_val),
                                     nn.Conv2d(in_channels= 128, out_channels=128 , kernel_size = (3,3), stride = 1, padding = 1,bias= False),
                                     Normalize(norm_type = self.norm_type ,num_features= 128),
-                                    nn.ReLU()
+                                    nn.ReLU(),
+                                    nn.Dropout2d(p = self.dropout_val)
                                     )
         
         self.layer2 = nn.Sequential(    
                                     nn.Conv2d(in_channels= 128, out_channels=256 , kernel_size = (3,3), stride = 1, padding = 1,bias= False),
                                     nn.MaxPool2d(kernel_size= (2,2),stride= 2),
                                     Normalize(norm_type = self.norm_type ,num_features= 256),
-                                    nn.ReLU()
+                                    nn.ReLU(),
+                                    nn.Dropout2d(p = self.dropout_val)
                                  )
         
         self.layer3 = nn.Sequential(    
                                     nn.Conv2d(in_channels= 256, out_channels=512 , kernel_size = (3,3), stride = 1, padding = 1,bias= False),
                                     nn.MaxPool2d(kernel_size= (2,2),stride= 2),
                                     Normalize(norm_type = self.norm_type ,num_features= 512),
-                                    nn.ReLU()
+                                    nn.ReLU(),
+                                    nn.Dropout2d(p = self.dropout_val)
                                     )
 
         self.ResBlock2 = nn.Sequential(    
                                     nn.Conv2d(in_channels= 512, out_channels=512 , kernel_size = (3,3), stride = 1, padding = 1,bias= False),
                                     Normalize(norm_type = self.norm_type ,num_features= 512),
                                     nn.ReLU(),
+                                    nn.Dropout2d(p = self.dropout_val),
                                     nn.Conv2d(in_channels= 512, out_channels=512 , kernel_size = (3,3), stride = 1, padding = 1,bias= False),
                                     Normalize(norm_type = self.norm_type ,num_features= 512),
-                                    nn.ReLU()
+                                    nn.ReLU(),
+                                    nn.Dropout2d(p = self.dropout_val)
                                     )
         # self.pool = nn.MaxPool2d(4,4
         self.pool = nn.AvgPool2d(kernel_size = (4,4),stride = 2)  
