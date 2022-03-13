86d410c44a422355dd5c3d4c88dcd5c63a980512
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2022-03-13 21:41:16 +0530

    Update TinyImageNet.py

--- utils/STRIX_datasets/TinyImageNet.py
+++ utils/STRIX_datasets/TinyImageNet.py
@@ -74,8 +74,7 @@ class TinyImageNet(Dataset):
                     im2 = cv2.merge((im,im,im))
                     self.data.append(im2)
             
-            # self.data = np.vstack(self.data).reshape(-1, 3,64, 64)
-            # self.data = np.vstack(self.data)
+            self.data = np.vstack(self.data).reshape(-1,3,64,64)
             # self.data = self.data.transpose((0, 2, 3, 1))  # convert to H
 
         else:
