dee44f79b03ca9881d0e74ebc1086eefeb32195c
Pruthiraj Jayasingh <data.pruthiraj@gmail.com>
2022-03-24 23:31:52 +0530

    Update resnet.py

--- model/resnet.py
+++ model/resnet.py
@@ -14,11 +14,9 @@ class BasicBlock(nn.Module):
 
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
@@ -72,9 +70,7 @@ class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -96,7 +92,7 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        out = F.adaptive_avg_pool2d(out, 512)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
@@ -111,18 +107,14 @@ def TinyNet_model():
 def ResNet34():
     return ResNet(BasicBlock, [3, 4, 6, 3])
 
-
 def ResNet50():
     return ResNet(Bottleneck, [3, 4, 6, 3])
-
 
 def ResNet101():
     return ResNet(Bottleneck, [3, 4, 23, 3])
 
-
 def ResNet152():
     return ResNet(Bottleneck, [3, 8, 36, 3])
-
 
 def test():
     net = ResNet18()
